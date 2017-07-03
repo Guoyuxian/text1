aa430d320a879379d00d336523076f2c485e4fa1
Guoyuxian <593483008@qq.com>
2017-07-03 11:54:23 +0800

    创建led.c文件
    第一次修改文件

---
+++ led.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+unsigned int  led1=1;
+unsigned char led2=0;
+void mian(){
+   while(1)
+   {
+     led1=1;
+     led1=0;
+     intit();
+     font();
+     sendString();
+     long();
+   }
+}
+void intit(){
+    
+}
